a55d8b158804669c9431b264b8ebf7c2c8d9d8a0
viethoang1211 <viethoang12112002@gmail.com>
2023-09-29 00:11:06 +0700

    small test

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', (req, res) => {
     const responseJson = {
-        message: 'Welcome warriors to Golden Owl! 2',
+        message: 'Welcome warriors to Golden Owl!',
     }
     res.json(responseJson)
 })
